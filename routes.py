7c34334b749e3105a32eecb8573019f9228c95bd
Jason <thebosssg19@gmail.com>
2022-08-26 17:57:51 -0400

    work already please?

--- routes.py
+++ routes.py
@@ -8,22 +8,23 @@ from utils.errors import UrlNotFoundError, UrlInvalidError
 from utils.templates import uri_not_found, uri_invalid
 app.encoder = ZeroWidthEncoder()
 log = getLogger(__name__)
-ILLIGAL_ROUTES = ['/add', '/', 'u.jasoncodes.ca']
+ILLIGAL_ROUTES = ['/add', '/'] #'u.jasoncodes.ca']
 
 @app.route('/<path:path>', methods=['GET'])
 @app.route('/u/<path:path>', methods=['GET'])
 def get_url(path: str):
     try:
         path = app.encoder.decode(path)
-        return_value = str(app.short.get_uri(path)).encode('utf-8')  # no need to specify encoding as its utf-8 by default
+        print(path)
+        return_value = str(app.short.get_uri(path)).encode()  # no need to specify encoding as its utf-8 by default
     except UrlNotFoundError:
         return uri_not_found(path)
     return redirect(return_value)
 
 
-@app.route('/')
-def main():
-    return redirect('https://jasoncodes.ca')
+#@app.route('/')
+#def main():
+#    return redirect('https://jasoncodes.ca')
 
 
 @app.route('/add/<path:url>', methods=['POST', 'GET', 'PUT'])
